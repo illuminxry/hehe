10d8d20e5989d140b4d95329ead451ab594dcd38
illuminxry <secret.ian0926@gmail.com>
2023-10-09 23:33:04 +0800

    added view for lesson

--- controller/lesson.js
+++ controller/lesson.js
@@ -16,6 +16,8 @@ exports.getLesson = (req, res) => {
     // SQL query to fetch quarterPeriod values from the "quarters" table
     const quarterSql = 'SELECT quarterID FROM quarter'; // Modify the table name as needed
 
+    const lessonSql = 'SELECT * FROM lesson';
+
     connection.connect((err) => {
         if (err) {
             console.error('Error connecting to the database:', err);
@@ -42,16 +44,26 @@ exports.getLesson = (req, res) => {
                         resolve(quarterResults);
                     }
                 });
+            }),
+            new Promise((resolve, reject) => {
+                connection.query(lessonSql, (err, lessonResults) => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve(lessonResults);
+                    }
+                });
             })
         ])
-            .then(([subjectResults, quarterResults]) => {
+            .then(([subjectResults, quarterResults, lessonResults]) => {
                 connection.end();
 
                 const subjectNames = subjectResults.map(result => result.subjectname);
                 const quarterID = quarterResults.map(result => result.quarterID);
+                const lessons = lessonResults;
 
                 // Pass subject names and quarterID to the template
-                res.render('lesson', { subjectNames, quarterID });
+                res.render('lesson', { subjectNames, quarterID, lessons });
             })
             .catch(err => {
                 console.error('Error fetching data from the database:', err);
@@ -61,13 +73,12 @@ exports.getLesson = (req, res) => {
 };
 
 exports.createLesson = (req, res) => {
-    const { lessonid, subjectname, quarterID, section1, section2, section3, section4, section5, section6, section7, section8, section9, section10 } = req.body;
+    const { lessonid, subjectname, quarterID, section1, section2, section3, section4, section5, section6, section7, section8, section9, section10, dateCreated, dateAvailableUntil, published } = req.body;
 
     const connection = mysql.createConnection(conn);
 
-    const sql = 'INSERT INTO lesson (lessonID, subjectname, quarterID, section1, section2, section3, section4, section5, section6, section7, section8, section9, section10) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
-    const values = [lessonid, subjectname, quarterID, section1, section2, section3, section4, section5, section6, section7, section8, section9, section10];
-
+    const sql = 'INSERT INTO lesson (lessonID, subjectname, quarterID, section1, section2, section3, section4, section5, section6, section7, section8, section9, section10, dateCreated, dateAvailableUntil, published) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
+    const values = [lessonid, subjectname, quarterID, section1, section2, section3, section4, section5, section6, section7, section8, section9, section10, dateCreated, dateAvailableUntil, published];
     connection.query(sql, values, (err, result) => {
         if (err) {
             console.error('Error executing SQL query:', err);
@@ -82,5 +93,48 @@ exports.createLesson = (req, res) => {
 
         // Redirect to the root URL ('/')
         res.redirect('/lesson');
+    });
+};
+
+
+exports.editLessonForm = (req, res) => {
+    const { id } = req.params;
+
+    Lesson.findById(id, (err, lesson) => {
+        if (err) {
+            console.error('Error finding lesson:', err);
+            res.status(500).send('Internal Server Error');
+        } else {
+            res.render('edit-lesson', { lesson });
+        }
+    });
+};
+
+exports.viewLesson = (req, res) => {
+    const { id } = req.params;
+
+    const connection = mysql.createConnection(conn);
+
+    // SQL query to fetch a single lesson by ID
+    const sql = `
+      SELECT lessonID, subjectname, quarterID, section1, section2, section3, section4, section5, section6, section7, section8, section9, section10, dateCreated, dateAvailableUntil, published
+      FROM lesson
+      WHERE ID = ?
+    `;
+
+    connection.query(sql, [id], (err, lesson) => {
+        if (err) {
+            console.error('Error fetching lesson data:', err);
+            res.status(500).send('Internal Server Error');
+            return;
+        }
+
+        connection.end();
+
+        if (!lesson || lesson.length === 0) {
+            res.status(404).send('Lesson not found');
+        } else {
+            res.render('view-lesson', { lesson: lesson[0] }); // Assuming you have a "view-lesson" EJS template
+        }
     });
 };
--- routes/lesson.js
+++ routes/lesson.js
@@ -6,4 +6,14 @@ const lessonController = require('../controller/lesson'); // Import the controll
 router.get('/lesson', lessonController.getLesson); // Use the controller for the route
 router.post('/lesson', lessonController.createLesson);
 
+// Display the form for editing a lesson
+// router.get('/lessons/:id/edit', lessonController.editLessonForm);
+
+// // Handle the update of a lesson
+// router.post('/lessons/:id/edit', lessonController.updateLesson);
+
+// // Handle the deletion of a lesson
+// router.post('/lessons/:id/delete', lessonController.deleteLesson);
+
+router.get('/lesson/view/:id', lessonController.viewLesson);
 module.exports = router;
